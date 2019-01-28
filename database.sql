c97b2eb2b24ce1595b8a40a8e1224212d8feee72
jvelilla <javier.hector@gmail.com>
2019-01-28 13:04:57 -0300

    Forgot to add database tables

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `artist` (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `country_id` int(11) DEFAULT NULL
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `country` (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `isocode` varchar(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
